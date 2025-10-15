bcb809784c19ff2aea7e9508ac383509eb234607
Roger Li <rel7817@nyu.edu>
2025-10-15 17:43:59 -0400

    cp

--- src/HullWhiteTreeUtil.py
+++ src/HullWhiteTreeUtil.py
@@ -1,7 +1,7 @@
 from src.HullWhiteTrinomialTree import OneFactorHullWhiteTrinomialTree, Node, LayerAttributesStruct
 import numpy as np
 from functools import wraps
-from src.VectorizedHullWhiteTrinomialTree import VectorizedHW1FTrinomialTree
+from src.VectorizedHullWhiteTrinomialTree import VectorizedHW1FTrinomialTree, LayerInfoCols
 
 # decorator for rounding list of floats to 4 digits...
 def round_list_floats(func):
@@ -19,7 +19,10 @@ class HullWhiteTreeUtil:
     @staticmethod
     def get_zcb_price_vector(tree: VectorizedHW1FTrinomialTree,
                              t0: float, T: float) -> np.ndarray:
-        
+        """
+        Get the discount factors for the horizon t0 to T.
+        """
+
         # round times
         t0 = round(t0, 4)
         T = round(T, 4)
@@ -39,7 +42,63 @@ class HullWhiteTreeUtil:
         if t0_row_index is None or T_row_index is None:
             raise Exception(f"Invalid t0 or T for the given tree.")
 
+<<<<<<< Updated upstream
         # 
+=======
+        cur_price = np.ones(tree.short_rate_tree.shape[1], dtype=float)
+
+        # backward induction step
+        parent_layer_index = T_row_index-1
+        while parent_layer_index > t0_row_index:
+
+            # mask for active nodes in current layer
+            parent_layer_mask   = tree.node_mask_tree[parent_layer_index]
+
+            # layer data
+            delta_t             = tree.layer_information[parent_layer_index, LayerInfoCols.DELTA_T]
+            layer_short_rates   = tree.short_rate_tree[parent_layer_index]
+
+            # indices for children
+            mid_child_index     = tree.mid_index_tree[parent_layer_index]
+            up_child_index      = mid_child_index + 1
+            down_child_index    = mid_child_index - 1
+
+            # probabilities for this layer
+            p_up                = tree.p_up_tree[parent_layer_index]
+            p_mid               = tree.p_mid_tree[parent_layer_index]
+            p_down              = tree.p_down_tree[parent_layer_index]
+
+            # child prices (next layer)
+            next_prices         = cur_price
+
+            # shift prices to align with parent layer nodes
+            price_up            = np.roll(next_prices, -1)
+            price_mid           = next_prices
+            price_down          = np.roll(next_prices, 1)
+
+            # zero out invalid child contributions due to tree truncation
+            # (using mask of next layer if available)
+            if parent_layer_index + 1 < tree.node_mask_tree.shape[0]:
+                next_mask = tree.node_mask_tree[parent_layer_index + 1]
+                price_up[~np.roll(next_mask, -1)] = 0.0
+                price_mid[~next_mask] = 0.0
+                price_down[~np.roll(next_mask, 1)] = 0.0
+
+            # expected discounted price at parent nodes
+            exp_factor = np.exp(-delta_t * layer_short_rates)
+            expected_price = (p_up * price_up + p_mid * price_mid + p_down * price_down) * exp_factor
+
+            # apply mask to ignore inactive nodes
+            cur_price = np.where(parent_layer_mask, expected_price, 0.0)
+
+            # move one layer up
+            parent_layer_index -= 1
+
+
+
+
+
+>>>>>>> Stashed changes
         # # backward induction
         # cur_layer: LayerAttributesStruct    = tree.t_to_layer[T].prev_layer_attr
         # last_layer: LayerAttributesStruct   = tree.t_to_layer[t0]
